3fc3854af97d19d30ea0bfc444ef5adaac4e37e2
Srajit549 <sjv5942@gmail.com>
2025-11-14 18:36:57 +0530

    Add files via upload

---
+++ Day2q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int l, b, a, p;
+    l = 30;
+    b = 40;
+    p = 2 * (l + b);
+    printf("The perimeter of the rectangle is: %d\n", p);
+    a = l * b;
+    printf("The area of the rectangle is: %d", a);
+    return 0; 
+}
