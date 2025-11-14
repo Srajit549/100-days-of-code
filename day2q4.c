ca8aa4f2c59fb26bcede898a9729fcf405ebcc13
Srajit549 <sjv5942@gmail.com>
2025-11-14 18:44:15 +0530

    Add files via upload

---
+++ day2q4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    
+    int r = 5;
+    float c = 2 * (22.0/7.0) * r;
+    float a = (22.0/7.0) * r * r;
+    printf("The circumference of the circle is: %f\n", c);
+    printf("The area of the circle is: %f", a);
+    return 0;
+}
